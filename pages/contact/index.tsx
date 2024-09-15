9a349c32846d01cb00074d3cc0f1b75b37cb47cd
bsspencer42 <mk8golfr2022@gmail.com>
2024-09-15 14:30:42 -0500

    Add CSS module for Contact page

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,16 +1,20 @@
 import Link from "next/link";
 import Head from "next/head";
+import styles from "../../styles/Contact.module.css";
 
 export default function Contact() {
   return (
-    <>
-      <Head>
-        <title>Contact</title>
-      </Head>
-      <h1>View source code on <a href='https://github.com/bsspencer42/AmplifyProject'>Github</a></h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
-    </>
+      <div className={styles.container}>
+        <Head>
+          <title>Contact</title>
+        </Head>
+        <h1>
+          View source code on{" "}
+          <a href="https://github.com/bsspencer42/AmplifyProject">Github</a>
+        </h1>
+        <h2>
+          <Link href="/">Back to home</Link>
+        </h2>
+      </div>
   );
 }
