ae799ae38aa51930c1e035719c0db9832c655d6b
bsspencer42 <mk8golfr2022@gmail.com>
2024-09-15 22:57:29 -0500

    Update todo list to utliize material ui components

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,21 +1,21 @@
 import Link from "next/link";
 import Head from "next/head";
 import styles from "../../styles/About.module.css";
-import { Typography } from "@mui/material"
+import { Typography, Container } from "@mui/material"
 
 export default function About() {
   return (
-    <div className={styles.container}>
+    <Container className={styles.container}>
       <Head>
         <title>Contact</title>
       </Head>
-      <Typography variant="h1">
-        View source code on{" "}
-        <a href="https://github.com/bsspencer42/AmplifyProject">Github</a>
-      </Typography>
-      <h2>
+        <Typography variant="h2" align="center" gutterBottom>
+          View source code on{" "}
+          <a href="https://github.com/bsspencer42/AmplifyProject">Github</a>
+        </Typography>
+      <Typography variant="h4">
         <Link href="/">Back to home</Link>
-      </h2>
-    </div>
+      </Typography>
+    </Container>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,19 @@ import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import Link from "next/link";
 import type { Schema } from "@/amplify/data/resource";
+import Button from "@mui/material/Button"
+import { Typography, Container, List, ListItemButton, Modal, Box } from "@mui/material";
+import { FormControl, InputLabel, Input } from "@mui/material";
+import styles from "../styles/App.module.css";
 
 const client = generateClient<Schema>();
 
 export default function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [openDelete, setOpenDelete] = useState<boolean>(false);
+  const [openCreate, setOpenCreate] = useState<boolean>(false);
+  const [deleteId, setDeleteId] = useState<string>("");
+  const [createContent, setCreateContent] = useState<string>("");
 
   function listTodos() {
     client.models.Todo.observeQuery().subscribe({
@@ -18,31 +26,108 @@ export default function App() {
     listTodos();
   }, []);
 
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
+  function openCreateTodoModal() {
+    setCreateContent("")
+    setOpenCreate(true)
   }
 
-  function deleteTodo(id: string) {
-    const shouldDelete = window.confirm("Delete this todo?");
-    shouldDelete && client.models.Todo.delete({ id });
+  function handleCreate() {
+    console.log("Create todo")
+    client.models.Todo.create({
+      content: createContent,
+    });
+    setCreateContent("");
+    setOpenCreate(false);
   }
+
+  function openDeleteTodoModal(id: string) {
+    setDeleteId(id);
+    setOpenDelete(true);
+  }
+
+  function handleDelete() {
+    const shouldDelete = deleteId !== "";
+    shouldDelete && client.models.Todo.delete({ id: deleteId });
+    setDeleteId("");
+    setOpenDelete(false)
+  }
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value)
+    setCreateContent(event.target.value);
+  };
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
+    <Container className={styles.container}>
+      <Typography variant="h2" color="default" align="center" gutterBottom>
+        Todo List
+      </Typography>
+      <Button
+        type="submit"
+        variant="contained"
+        onClick={openCreateTodoModal}
+        className={styles.button}
+      >
+        Create Todo
+      </Button>
+      <Modal open={openCreate} onClose={() => setOpenCreate(false)}>
+        <Box
+          sx={{
+            position: "absolute" as "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 400,
+            bgcolor: "background.paper",
+            border: "2px solid #000",
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
+          <FormControl>
+            <InputLabel>Todo Item</InputLabel>
+            <Input
+              value={createContent}
+              onChange={handleChange}
+              aria-describedby="input-helper-text"
+            />
+            <Button onClick={() => handleCreate()}>Submit Todo</Button>
+          </FormControl>
+        </Box>
+      </Modal>
+      <List className={styles.list}>
         {todos.map((todo) => (
-          <li key={todo.id} onClick={() => deleteTodo(todo.id)}>
+          <ListItemButton
+            key={todo.id}
+            onClick={() => openDeleteTodoModal(todo.id)}
+            className={styles.listItem}
+          >
             {todo.content}
-          </li>
+          </ListItemButton>
         ))}
-      </ul>
-      <h1>
+      </List>
+      <Modal open={openDelete} onClose={() => setOpenDelete(false)}>
+        <Box
+          sx={{
+            position: "absolute" as "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 400,
+            bgcolor: "background.paper",
+            border: "2px solid #000",
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
+          <Typography variant="h4">Delete Todo Item?</Typography>
+          <Button onClick={() => handleDelete()}>Yes</Button>
+          <Button onClick={() => setOpenDelete(false)}>No</Button>
+        </Box>
+      </Modal>
+      <Typography variant="h4" className={styles.about}>
         <Link href="/about">About the author</Link>
-      </h1>
-    </main>
+      </Typography>
+    </Container>
   );
 }
